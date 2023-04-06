ac564083fb45b6b857fe38d34764c92776f2a057
Josh Merrill <joshmmerrill@outlook.com>
2023-04-06 07:43:49 +0100

    REFACTOR: Updated card component props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,11 @@ export default function Card(props) {
     return (
         <div className="card--wrapper">
         <div className="card--container">
-            <img src={`${props.imageUrl}`} className="card--image" />
+            <img src={props.imageUrl} className="card--image" />
             <div className="card--details">
                 <div className="card--location_details">
                     <p className="card--location"><i className="fas fa-map-marker-alt"></i> {props.location}</p>
-                    <a href={`${props.googleMapsUrl}`} target="_blank" className="card--link">View on Google Maps</a>
+                    <a href={{props.googleMapsUrl}} target="_blank" className="card--link">View on Google Maps</a>
                 </div>
                 <h2 className="card--title">{props.title}</h2>
                 <h2 className="card--dates">{props.startDate} - {props.endDate}</h2>
